21968239087f3e9b5e211381b2ddf042a017ae18
gotcha-123 <torrez.pedro.160390@gmail.com>
2024-08-16 00:51:43 -0600

    12th commit | template inheritance

--- DjangoSolution01/tasks/views.py
+++ DjangoSolution01/tasks/views.py
@@ -10,3 +10,7 @@ def index(request):
     return render(request, "tasks/index.html", {
         "tasks": tasks
     })
+
+# Add a new task:
+def add(request):
+    return render(request, "tasks/add.html")

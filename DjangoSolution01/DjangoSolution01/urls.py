badaf609ee0c2b9de3bffefa033d49e13ac43f19
gotcha-123 <torrez.pedro.160390@gmail.com>
2024-08-16 00:37:04 -0600

    11th commit | tasks for loop

--- DjangoSolution01/DjangoSolution01/urls.py
+++ DjangoSolution01/DjangoSolution01/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('default_app/', include("default_app.urls")),
-    path('newyear/', include("newyear.urls"))
+    path('newyear/', include("newyear.urls")), 
+    path('tasks/', include("tasks.urls"))
 ]

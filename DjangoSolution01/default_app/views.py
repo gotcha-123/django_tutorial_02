977d6a07a19527c6406f702f37c3043e5874ae64
gotcha-123 <torrez.pedro.160390@gmail.com>
2024-08-15 21:08:50 -0600

    5th commit

--- DjangoSolution01/default_app/views.py
+++ DjangoSolution01/default_app/views.py
@@ -13,4 +13,4 @@ def david(request):
     return HttpResponse("Hello, David!")
 
 def greet(request, name):
-    return HttpResponse(f"Hello, {name.capitalize()}!")
+    return HttpResponse("<h1 style=\"color:blue\">Hello, world!</h1>")
